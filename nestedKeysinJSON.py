c98da2a5e56650b9195c2d17bbe760dfbe68309e
ashisht411 <ashishsohar@gmail.com>
2025-06-16 12:59:13 +0530

    newFile

---
+++ nestedKeysinJSON.py
@@ -0,0 +1,21 @@
+import json
+
+def extract_nested_keys(data, parent_key=""):
+    keys = []
+    for key, value in data.items():
+        full_key = f"{parent_key}.{key}" if parent_key else key
+        if isinstance(value, dict):
+            keys.extend(extract_nested_keys(value, full_key))
+        else:
+            keys.append(full_key)
+    return keys
+
+def get_nested_keys_from_json_file(file_path):
+  with open(file_path, "r") as file:
+    data = json.load(file)
+
+  nested_keys = extract_nested_keys(data)
+  return nested_keys
+
+filepath = "happy.json"
+print(get_nested_keys_from_json_file(filepath))
